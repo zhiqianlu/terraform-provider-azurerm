d96d91f6ff8883d66ada4ad438cc488552766317
jackofallops <ste@hashicorp.com>
2021-11-25 18:14:51 +0000

    small refactor

--- internal/tools/schema-api/providerjson/handlers.go
+++ internal/tools/schema-api/providerjson/handlers.go
@@ -5,9 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
-
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 const (
@@ -61,14 +58,4 @@ func (p *ProviderJSON) ListDataSources(w http.ResponseWriter, _ *http.Request) {
 	if err := json.NewEncoder(w).Encode(p.DataSources()); err != nil {
 		panic(err)
 	}
-}
-
-func (p *ProviderJSON) DataSources() []terraform.DataSource {
-	s := schema.Provider(*p)
-	return s.DataSources()
-}
-
-func (p *ProviderJSON) Resources() []terraform.ResourceType {
-	s := schema.Provider(*p)
-	return s.Resources()
 }
